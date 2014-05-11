a9995c695a01a75a21b079f10b9924f6dc7b95eb
Jared Szechy <jared.szechy@brilligent.com>
2014-05-10 21:07:00 -0400

    Add sudo to install

--- travis_deps.sh
+++ travis_deps.sh
@@ -10,7 +10,7 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
-make install
+sudo make install
 
 # Install IT++ from SF
 cd /tmp
@@ -21,4 +21,4 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=/usr ..
 make
-make install
+sudo make install
