7ee04e58c5b18d42fbb2b2138246f7d6f380b6fc
Jared Szechy <jared.szechy@gmail.com>
2015-08-06 12:54:09 -0400

    Use the new travis containers

--- travis_deps.sh
+++ travis_deps.sh
@@ -2,23 +2,25 @@
 set -ex
 
 # Install mbelib
-cd /tmp
-wget https://github.com/szechyjs/mbelib/archive/master.tar.gz -O /tmp/mbelib.tar.gz
+wget https://github.com/szechyjs/mbelib/archive/master.tar.gz -O mbelib.tar.gz
 tar -xzf mbelib.tar.gz
 cd mbelib-master
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DCMAKE_INSTALL_PREFIX=$HOME/mbelib ..
 make
-sudo make install
+make install
 
 # Install IT++ from SF
-cd /tmp
-wget http://sourceforge.net/projects/itpp/files/itpp/4.3.1/itpp-4.3.1.tar.gz/download -O /tmp/itpp-4.3.1.tar.gz
-tar -xzf itpp-4.3.1.tar.gz
-cd itpp-4.3.1
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr ..
-make
-sudo make install
+if [ ! -d "$HOME/itpp/lib" ]; then
+  wget http://sourceforge.net/projects/itpp/files/itpp/4.3.1/itpp-4.3.1.tar.gz/download -O itpp-4.3.1.tar.gz
+  tar -xzf itpp-4.3.1.tar.gz
+  cd itpp-4.3.1
+  mkdir build
+  cd build
+  cmake -DCMAKE_INSTALL_PREFIX=$HOME/itpp ..
+  make
+  make install
+else
+  echo 'Using cached itpp.';
+fi
