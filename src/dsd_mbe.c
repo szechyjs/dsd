663caa74fec1eb23dd93b227b0735f4061a4983d
Patrick Eads <peads@users.noreply.github.com>
2023-02-07 08:15:51 -0500

    Refactored dsd_mbe::playMbeFile to properly handle eof and closing the currently read file, which was causing segfaults for large numbers of mbe files being read in. Extracted reader function calls to typedefed pointer, s.t. since the file type only needs to be read per file, it could be set to a generic function pointer thus cleaning up the read loop. Fixed end conditions for both the while and for loops, s.t. they break on exit flag, and their respective conditions. Removed redundant cleanup function call since the caller calls it on return of the play function anyway.

--- src/dsd_mbe.c
+++ src/dsd_mbe.c
@@ -17,56 +17,84 @@
 
 #include "dsd.h"
 
-void
-playMbeFiles (dsd_opts * opts, dsd_state * state, int argc, char **argv)
-{
+typedef void (*dataReaderDef)(dsd_opts *opts, dsd_state *state);
 
-  int i;
-  char imbe_d[88];
-  char ambe_d[49];
+void readImbe(dsd_opts *opts, dsd_state *state) {
 
-  for (i = state->optind; i < argc; i++)
-    {
-      sprintf (opts->mbe_in_file, "%s", argv[i]);
-      openMbeInFile (opts, state);
-      mbe_initMbeParms (state->cur_mp, state->prev_mp, state->prev_mp_enhanced);
-      printf ("playing %s\n", opts->mbe_in_file);
-      while (feof (opts->mbe_in_f) == 0)
-        {
-          if (state->mbe_file_type == 0)
-            {
-              readImbe4400Data (opts, state, imbe_d);
-              mbe_processImbe4400Dataf (state->audio_out_temp_buf, &state->errs, &state->errs2, state->err_str, imbe_d, state->cur_mp, state->prev_mp, state->prev_mp_enhanced, opts->uvquality);
-              processAudio (opts, state);
-              if (opts->wav_out_f != NULL)
-                {
-                  writeSynthesizedVoice (opts, state);
-                }
+    char imbe_d[88];
+    readImbe4400Data(opts, state, imbe_d);
+    mbe_processImbe4400Dataf(state->audio_out_temp_buf,
+                             &state->errs,
+                             &state->errs2,
+                             state->err_str,
+                             imbe_d,
+                             state->cur_mp,
+                             state->prev_mp,
+                             state->prev_mp_enhanced,
+                             opts->uvquality);
+}
 
-              if (opts->audio_out == 1)
-                {
-                  playSynthesizedVoice (opts, state);
-                }
+void readAmbe(dsd_opts *opts, dsd_state *state) {
+
+    char ambe_d[49];
+    readAmbe2450Data(opts, state, ambe_d);
+    mbe_processAmbe2450Dataf(state->audio_out_temp_buf,
+                             &state->errs,
+                             &state->errs2,
+                             state->err_str,
+                             ambe_d,
+                             state->cur_mp,
+                             state->prev_mp,
+                             state->prev_mp_enhanced,
+                             opts->uvquality);
+}
+
+void playMbeFiles(dsd_opts *opts, dsd_state *state, int argc, char **argv) {
+
+    int i;
+    dataReaderDef readData = NULL;
+
+    for (i = state->optind; !exitflag && i < argc; i++) {
+
+        strcpy(opts->mbe_in_file, argv[i]);
+        openMbeInFile(opts, state);
+        mbe_initMbeParms(state->cur_mp,
+                         state->prev_mp,
+                         state->prev_mp_enhanced);
+        printf("playing %s\n", opts->mbe_in_file);
+
+        switch (state->mbe_file_type) {
+            case 0:
+                readData = readImbe;
+                break;
+            case 1:
+                readData = readAmbe;
+                break;
+        }
+
+        while (!exitflag) {
+
+            readData(opts, state);
+            processAudio(opts, state);
+
+            if (opts->wav_out_f != NULL) {
+                writeSynthesizedVoice(opts, state);
             }
-          else if (state->mbe_file_type == 1)
-            {
-              readAmbe2450Data (opts, state, ambe_d);
-              mbe_processAmbe2450Dataf (state->audio_out_temp_buf, &state->errs, &state->errs2, state->err_str, ambe_d, state->cur_mp, state->prev_mp, state->prev_mp_enhanced, opts->uvquality);
-              processAudio (opts, state);
-              if (opts->wav_out_f != NULL)
-                {
-                  writeSynthesizedVoice (opts, state);
-                }
-              if (opts->audio_out == 1)
-                {
-                  playSynthesizedVoice (opts, state);
-                }
+
+            if (1 == opts->audio_out) {
+                playSynthesizedVoice(opts, state);
             }
-          if (exitflag == 1)
-            {
-              cleanupAndExit (opts, state);
+
+            if (ferror(opts->mbe_in_f)) {
+                perror("I/O error when reading");
+                exitflag = 1;
+                break;
+            } else if (feof(opts->mbe_in_f)) {
+                break;
             }
         }
+
+        closeMbeOutFile(opts, state);
     }
 }
 
